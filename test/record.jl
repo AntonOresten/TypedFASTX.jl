f4d1b9653aaa66662d5c57e8e1902d9b77c3e55d
anton083 <anton.sollman@outlook.com>
2023-08-01 19:18:16 +0200

    Update record.jl

--- test/record.jl
+++ test/record.jl
@@ -33,46 +33,4 @@
     end
 
     @test summary(DNARecord("", "")) == "TypedFASTX.TypedFASTA.Record{LongSequence{DNAAlphabet{4}}}"
-
-    #=
-
-    rec3 = TypedRecord("Morty", "Smith", "!!!!!")
-    @test rec3 isa StringRecord
-    @test AARecord(rec3) == TypedRecord("Morty", aa"SMITH", "!!!!!")
-    
-    rec4 = DNARecord("", "ACGT", "!!!!")
-    rec5 = DNARecord("", "ACTG", "~~~~")
-    @test all(==(1.0), error_prob_generator(rec4))
-    @test all(<(1e-5), error_prob_generator(rec5))
-
-    @test error_probs(rec4) == collect(error_prob_generator(rec4))
-
-    rec6 = DNARecord("", "ACGT", QualityScores(";;;;", :solexa))
-    @test all(>(0.75), error_prob_generator(rec6))
-
-    rec7 = DNARecord("Ricky", "ACGT")
-    io = IOBuffer()
-    Base.print(io, rec7)
-    output = String(take!(io))
-    @test output == ">Ricky\nACGT"
-
-    rec8 = DNARecord("Ricky", "ACGT", "!!!!")
-    io = IOBuffer()
-    Base.print(io, rec8)
-    output = String(take!(io))
-    @test output == "@Ricky\nACGT\n+\n!!!!"
-
-    @test sprint(show, rec0) == "DNARecord(\"Ricky\", \"ACGT\")"
-    @test sprint(show, rec1) == "DNARecord(\"Ricky\", \"ACGT\", \"!!!!\")"
-
-    io = IOBuffer()
-    Base.invokelatest(show, io, MIME("text/plain"), DNARecord("Ricky", "ACGT"))
-    str = String(take!(io))
-    @test str == "DNARecord:\n description: \"Ricky\"\n   sequence: \"ACGT\""
-
-    io = IOBuffer()
-    Base.invokelatest(show, io, MIME("text/plain"), DNARecord("Ricky", "ACGT", "!!!!"))
-    str = String(take!(io))
-    @test str == "DNARecord:\n description: \"Ricky\"\n   sequence: \"ACGT\"\n    quality: \"!!!!\""
-    =#
 end
