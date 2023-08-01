426efe7719613bb506abe4943e623212e68d9662
anton083 <anton.sollman@outlook.com>
2023-08-01 13:47:03 +0200

    Update record.jl

--- src/FASTQ/record.jl
+++ src/FASTQ/record.jl
@@ -69,25 +69,10 @@ function Base.print(io::IO, record::Record{T}) where T
     print(io, "@$(description(record))\n$(sequence(record))\n+\n$(quality(record))\n")
 end
 
-"""
-    error_prob_generator(record::Record)
-
-Creates a generator of error probabilities for each character in the sequence.
-"""
 error_prob_generator(record::Record) = error_prob_generator(record.quality)
 
-"""
-    error_probs(record::Record)
-
-Creates a vector of error probabilities for each character in the sequence.
-"""
 error_probs(record::Record) = error_probs(record.quality)
 
 import ..TypedFASTX: error_rate
 
-"""
-    error_rate(record::Record)
-
-Calculates the error rate of the record.
-"""
 error_rate(record::Record) = error_rate(record.quality)
